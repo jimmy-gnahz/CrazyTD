962ec8f52519eea72266578773ec485cd85611ed
dangeroustuna <gaccount@126.com>
2013-12-17 23:22:33 -0800

    some getter and setter for block

--- TD/src/crazytd/map/Block.java
+++ TD/src/crazytd/map/Block.java
@@ -1,5 +1,38 @@
 package crazytd.map;
 
-public class Block {
+import org.robobrain.sdk.graphics.Vector;
 
+
+public class Block {
+	public enum blockType{
+		BUILDABLE , ROAD, WASTELAND, OBSTACLES		
+	}
+	
+	private int x;		// location
+	private int y;		// location
+	private blockType type;//block type
+	public Block(int x, int y, blockType type){
+		this.x = x;
+		this.y = y;
+		this.type=type;
+	}
+	public Block(float x, float y, blockType type){
+		this.x= Math.round(x);
+		this.y= Math.round(y);
+		this.type = type;
+	}
+	public Vector getCondinate(){
+		return new Vector((float)x,(float)y);
+	}
+	
+	public float getX(){
+		return (float)x;
+	}
+	
+	public float getY(){
+		return (float)y;
+	}
+	public blockType getBlockType(){
+		return type;
+	}
 }
