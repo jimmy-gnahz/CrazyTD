2a4092b8e2c70bd15135d0618d9e382f69199f4b
dangeroustuna <gaccount@126.com>
2013-12-18 23:03:49 -0800

    modified font game so we can use it for printout

--- TD/src/crazytd/map/Block.java
+++ TD/src/crazytd/map/Block.java
@@ -5,21 +5,23 @@ import org.robobrain.sdk.graphics.Vector;
 
 public class Block {
 	public enum blockType{
-		BUILDABLE , ROAD, WASTELAND, OBSTACLES		
+		BUILDABLE , ROAD, WASTELAND, OBSTACLES
 	}
 	
 	private int x;		// location
 	private int y;		// location
 	private blockType type;//block type
-	public Block(int x, int y, blockType type){
+	
+	public Block(int x, int y, String type){
 		this.x = x;
 		this.y = y;
-		this.type=type;
+		this.type=blockType.valueOf(type);
 	}
-	public Block(float x, float y, blockType type){
-		this.x= Math.round(x);
-		this.y= Math.round(y);
-		this.type = type;
+
+	public Block(double x, double y, String type){
+		this.x=(int)Math.round(x);
+		this.y=(int)Math.round(y);
+		this.type=blockType.valueOf(type);
 	}
 	public Vector getCondinate(){
 		return new Vector((float)x,(float)y);
@@ -32,7 +34,7 @@ public class Block {
 	public float getY(){
 		return (float)y;
 	}
-	public blockType getBlockType(){
-		return type;
+	public String getBlockType(){
+		return type.toString();
 	}
 }
--- TD/src/org/robobrain/test/FontGame.java
+++ TD/src/org/robobrain/test/FontGame.java
@@ -8,7 +8,7 @@ import org.robobrain.sdk.game.World;
 import org.robobrain.sdk.graphics.TextSprite;
 import org.robobrain.sdk.graphics.Texture;
 import org.robobrain.sdk.graphics.TextureManager;
-
+import crazytd.map.Block;
 public class FontGame extends Engine {
 	public static final int FONT_TEXTURE = 2;
 	
@@ -17,10 +17,13 @@ public class FontGame extends Engine {
 		TextureManager.registerTexture("images/arial.png", FONT_TEXTURE);
 		
 		mWorld = new World();
-		TextEntity te = new TextEntity();
-		te.setX(0);
-		te.setY(100);
+		Block b = new Block(1.75,3.14,"ROAD");
+		TextEntity te = new TextEntity(0,0.00*mWorld.getHeight(),b.getBlockType());
 		mWorld.addEntity(te);
+		TextEntity num1 = new TextEntity(0,0.05*mWorld.getHeight(),b.getX()+"");
+		mWorld.addEntity(num1);
+		TextEntity num2 = new TextEntity(0,0.10*mWorld.getHeight(),b.getY()+"");
+		mWorld.addEntity(num2);
 	}
 }
 
@@ -28,10 +31,18 @@ class TextEntity extends Entity {
 	public TextEntity() {
 		Texture tex = TextureManager.getTexture(FontGame.FONT_TEXTURE);
 		TextSprite ts = new TextSprite(tex);
-		ts.setMessage("Hello, Fonts!");
+		ts.setMessage("hello Font");
 		mRenderable = ts;
 	}
 	
+	public TextEntity(double x, double y,String s){
+		Texture tex = TextureManager.getTexture(FontGame.FONT_TEXTURE);
+		TextSprite ts = new TextSprite(tex);
+		ts.x=(float)x;
+		ts.y=(float)y;
+		ts.setMessage(s);
+		mRenderable = ts;
+	}
 	@Override 
 	public void update(long time) {
 		// Don't animate me...
